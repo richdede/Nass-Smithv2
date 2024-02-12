2867a98ac1ccf6d9698cf34e37ed4c53f8479f62
richdede <deeprince2002@gmail.com>
2024-02-12 02:54:52 -0800

    screen

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,26 +4,31 @@ import { Link } from "react-router-dom"
 import { navlink } from "../data/dummydata"
 
 export const Header = () => {
-  const [responsive, setResponsive] = useState(false)
+  const [responsive, setResponsive] = useState(false);
+
+  const handleLinkClick = () => {
+    if (responsive) {
+      setResponsive(false);
+    }
+  }
+
   return (
-    <>
-      <header>
-        <div className='container flexsb'>
-          <Link to="/" className='logo'>
+    <header>
+      <div className='container flexsb'>
+        <Link to="/" className='logo'>
           Nass smith
-          </Link>
-          <div className={responsive ? "hideMenu" : "nav"}>
-            {navlink.map((links, i) => (
-              <Link to={links.url} key={i} data-aos='zoom-in-left'>
-                {links.text}
-              </Link>
-            ))}
-          </div>
-          <button className='toggle' onClick={() => setResponsive(!responsive)}>
-            <Menu className='icon' />
-          </button>
+        </Link>
+        <div className={responsive ? "hideMenu" : "nav"}>
+          {navlink.map((links, i) => (
+            <Link to={links.url} key={i} data-aos='zoom-in-left' onClick={handleLinkClick}>
+              {links.text}
+            </Link>
+          ))}
         </div>
-      </header>
-    </>
+        <button className='toggle' onClick={() => setResponsive(!responsive)}>
+          <Menu className='icon' />
+        </button>
+      </div>
+    </header>
   )
 }
